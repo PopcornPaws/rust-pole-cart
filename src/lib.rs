8dec4abba5430749b2d286f27983681943f4698a
Mark Melczer <melczer7@gmail.com>
2021-06-26 20:27:20 +0200

    wip.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,13 @@ use kiss3d::scene::SceneNode;
 use kiss3d::window::Window;
 
 const POLE_HALF_LENGTH: f32 = 1.0;
+const POLE_LENGTH: f32 = 2.0 * POLE_HALF_LENGTH;
 const POLE_Z_SHIFT: f32 = POLE_HALF_LENGTH + 0.11;
+const POLE_MASS: f32 = 0.1;
+const CART_MASS: f32 = 1.0;
+const ALL_MASS: f32 = CART_MASS + POLE_MASS;
+const POLE_X_INERTIA: f32 = POLE_MASS * POLE_HALF_LENGTH / 3.0;
+const G: f32 = 9.81;
 
 pub struct KissScene {
     camera: ArcBall,
@@ -33,7 +39,7 @@ impl Default for KissScene {
         let mut cart = window.add_cube(0.8, 1.2, 0.2);
         cart.set_color(0.5, 0.1, 0.7);
 
-        let mut pole = window.add_cube(0.05, 0.05, 2.0 * POLE_HALF_LENGTH);
+        let mut pole = window.add_cube(0.05, 0.05, POLE_LENGTH);
         pole.set_color(0.1, 0.5, 0.4);
         pole.set_local_translation(Translation3::<f32>::new(0.0, 0.0, POLE_Z_SHIFT));
 
@@ -80,5 +86,22 @@ impl KissScene {
             .draw_line(origin, end_k, &Point3::new(0.0, 0.0, 1.0));
 
         self.window.render_with_camera(&mut self.camera)
+    }
+}
+
+#[derive(Default)]
+pub struct State {
+    cart_position: f32,
+    cart_velocity: f32,
+    pole_angle: f32,
+    pole_angular_velocity: f32,
+}
+
+// returns cart position and pole angle
+impl State {
+    pub fn dynamics(&mut self, input_force: f32, dt: f32) {
+        let cart_forces = 
+        self.cart_position += self.cart_velocity * dt;
+        self.cart_velocity += force * dt / ALL_MASS;
     }
 }
